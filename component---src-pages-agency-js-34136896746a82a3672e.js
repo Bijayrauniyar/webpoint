5b0fcc7095a3b2968489ce2cdf9451fb5a06fef5
Bijayrauniyar <rauniyarbijay117@gmail.com>
2020-08-26 22:27:52 +0545

    Updates

---
+++ component---src-pages-agency-js-34136896746a82a3672e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{cOfG:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),c=t.n(a),l=t("Bl7J"),r=t("vrFN");n.default=function(){return c.a.createElement(l.a,null,c.a.createElement(r.a,{title:"Agnecy"}),c.a.createElement("h1",null,"Agnecy page"))}}}]);
+//# sourceMappingURL=component---src-pages-agency-js-34136896746a82a3672e.js.map
