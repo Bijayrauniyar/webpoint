20668233e756646000397e2e00adb9f6762d73e7
PrabhakarShrestha <shrestha.prabhakar440@gmail.com>
2020-08-29 16:42:51 +0545

    all porfolio design pages added

--- src/pages/fhcb.js
+++ src/pages/fhcb.js
@@ -2,11 +2,20 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Portfolio from "../components/Portfolio/portfolio"
 
 const FHCB = () => (
   <Layout>
     <SEO title="FHCB" />
-    <h1>FHCB page</h1>
+    <Portfolio
+      projectName="Family Home Christian Books"
+      projectSummary="Family Home Christian Books is a e-commerce bookstore that has been enriching families with books, DVDs, and other materials."
+      bannerImage="/images/fhcb-banner.png"
+      goalsContent="First of all, we developed a design system, so that all products of the company have a common style and adhere to the same rules in design. The interface has received significant improvements. Now the main functions have become more accessible, and thanks to the responsive design the site is convenient to use on any device."
+      processImage="/images/fhcb-process.png"
+      resultContent="First of all, we developed a design system, so that all products of the company have a common style and adhere to the same rules in design. The interface has received significant improvements. Now the main functions have become more accessible, and thanks to the responsive design the site is convenient to use on any device."
+      nextPage="/corniche"
+    ></Portfolio>
   </Layout>
 )
 
--- src/pages/jeewit-bachan.js
+++ src/pages/jeewit-bachan.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Portfolio from "../components/Portfolio/portfolio"
 
 const JeewitBachan = () => (
   <Layout>
@@ -9,7 +10,14 @@ const JeewitBachan = () => (
       title="Jeewit Bachan
     "
     />
-    <h1>Jeewit Bachan page</h1>
+    <Portfolio
+      projectName="Jeewit Bachan"
+      projectSummary="Plate App is a service for delivering your favorite food from restaurants on your mobile phone."
+      bannerImage="/images/Jeewit-Bachan-banner.png"
+      goalsContent="First of all, we developed a design system, so that all products of the company have a common style and adhere to the same rules in design. The interface has received significant improvements. Now the main functions have become more accessible, and thanks to the responsive design the site is convenient to use on any device."
+      processImage="/images/Jeewit-Bachan-process.png"
+      resultContent="First of all, we developed a design system, so that all products of the company have a common style and adhere to the same rules in design. The interface has received significant improvements. Now the main functions have become more accessible, and thanks to the responsive design the site is convenient to use on any device."
+    ></Portfolio>
   </Layout>
 )
 
